7dab85964184bad21f8a1ac9ab1df80e55900edf
Damien Plénard <damien@plenard.me>
2024-06-20 09:35:29 +0200

    feat: output ssh key id

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output "kubeconfig" {
   description = "Value of the kubeconfig"
   sensitive   = true
 }
+
+output "ssh_key_id" {
+  value       = module.kube.ssh_key_id
+  description = "The ID of the SSH key on Hetzner Cloud."
+}
